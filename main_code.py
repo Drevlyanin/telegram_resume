e831885e75e87b2facccb9e01bdc3638e0742444
Drevlyanin <121987689+Drevlyanin@users.noreply.github.com>
2023-04-05 00:01:56 +0300

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -1,9 +1,1 @@
-import telebot
 
-bot = telebot.TeleBot('your_token_here')
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет! Я твоё резюме. Как я могу тебе помочь?")
-
-bot.polling()
