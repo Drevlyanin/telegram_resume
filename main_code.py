eaa9bdfdd697fb473ae61ad9198b16bdc740614b
Drevlyanin <121987689+Drevlyanin@users.noreply.github.com>
2023-04-13 22:48:06 +0300

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -1,1 +1,9 @@
+import pyrogram
 
+app = pyrogram.Client('my_resume_bot', api_id='your_api_id_here', api_hash='your_api_hash_here')
+
+@app.on_message(pyrogram.Filters.command('start'))
+def send_welcome(client, message):
+client.send_message(message.chat.id, "Привет! Я твоё резюме. Как я могу тебе помочь?")
+
+app.run()
