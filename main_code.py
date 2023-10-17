ccb35d0e26f2cef3bb524a2b05f6aeb40060703e
Drevlyanin <121987689+Drevlyanin@users.noreply.github.com>
2023-10-17 18:39:22 +0300

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -3,7 +3,6 @@ from pyrogram import InlineKeyboardMarkup, InlineKeyboardButton
 
 app = pyrogram.Client('my_resume_bot', api_id='your_api_id_here', api_hash='your_api_hash_here')
 
-# Variables to track interaction count and user IDs
 interactions_count = 0
 user_ids = []
 
@@ -11,13 +10,10 @@ user_ids = []
 def send_welcome(client, message):
     global interactions_count, user_ids
 
-    # Increase interaction count on each interaction
     interactions_count += 1
 
-    # Record user ID of the user interacting with the bot
     user_ids.append(message.chat.id)
 
-    # Send a welcome message to the user with a help button
     help_button = InlineKeyboardButton('Help', callback_data='help')
     reply_markup = InlineKeyboardMarkup([[help_button]])
     client.send_message(message.chat.id, "Hello! I am your resume bot. How can I help you?", reply_markup=reply_markup)
@@ -26,7 +22,6 @@ def send_welcome(client, message):
 def send_stats(client, message):
     global interactions_count, user_ids
 
-    # Check if the stats request is coming from the bot owner (you can define your own user ID)
     if message.chat.id == your_user_id:
         # Send statistics to the bot owner
         stats = f"Interactions count: {interactions_count}\nUser IDs: {user_ids}"
@@ -40,4 +35,3 @@ def handle_callback_query(client, callback_query):
         client.send_message(callback_query.message.chat.id, help_message)
 
 app.run()
-
